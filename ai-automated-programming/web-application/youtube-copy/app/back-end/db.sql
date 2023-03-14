099a9e0e8ec1029c4b47023f44295f7f64485301
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-14 18:33:40 -0400

    update database tables settings and insert new test data

--- ai-automated-programming/web-application/youtube-copy/app/back-end/db.sql
+++ ai-automated-programming/web-application/youtube-copy/app/back-end/db.sql
@@ -1,3 +1,4 @@
+DROP DATABASE video_website;
 CREATE DATABASE video_website;
 USE video_website;
 
@@ -5,12 +6,13 @@ CREATE TABLE videos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   author VARCHAR(255) NOT NULL,
-  thumbnail VARCHAR(255) NOT NULL
+  thumbnail VARCHAR(255) NOT NULL,
+  url VARCHAR(255) NOT NULL
 );
 
-INSERT INTO videos (title, author, thumbnail)
-VALUES ('Sample Video 1', 'John Doe', 'https://via.placeholder.com/300x200'),
-       ('Sample Video 2', 'Jane Doe', 'https://via.placeholder.com/300x200');
+INSERT INTO videos (title, author, thumbnail, url)
+VALUES ('Sample Video 1', 'John Doe', 'https://via.placeholder.com/300x200', 'https://www.youtube.com/watch?v=outcGtbnMuQ'),
+       ('Sample Video 2', 'Jane Doe', 'https://via.placeholder.com/300x200', 'https://www.youtube.com/watch?v=SGUCcjHTmGY');
 
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
