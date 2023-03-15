487db8bda92a7e0674eadd31816efd5e38b825ce
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-15 03:28:02 -0400

    add trending module in front-end

---
+++ ai-automated-programming/web-application/youtube-copy/app/front-end/trending.js
@@ -0,0 +1,62 @@
+const apiKey = "AIzaSyCKL6mMvN2bmXYL6jD_FYA8oTednKfv5HU";
+
+fetchTrendingVideos();
+
+function fetchTrendingVideos() {
+  const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&order=viewCount&publishedAfter=${getTwoHoursAgo()}&maxResults=50&key=${apiKey}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const videoGrid = document.getElementById("video-grid");
+      videoGrid.innerHTML = "";
+
+      const videoIds = data.items.map((item) => item.id.videoId).join(",");
+
+      fetchVideoStats(videoIds).then((videoStats) => {
+        let displayedVideos = 0;
+        data.items.forEach((item, index) => {
+          if (
+            videoStats[index].statistics.likeCount > 5000 &&
+            displayedVideos < 10
+          ) {
+            videoGrid.innerHTML += `
+              <div class="col-md-4 mb-4">
+                <div class="card">
+                  <a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank">
+                    <img src="${item.snippet.thumbnails.high.url}" class="card-img-top" alt="Thumbnail">
+                  </a>
+                  <div class="card-body">
+                    <h5 class="card-title">${item.snippet.title}</h5>
+                    <p class="card-text">${item.snippet.description}</p>
+                  </div>
+                </div>
+              </div>
+            `;
+            displayedVideos++;
+          }
+        });
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching trending videos:", error);
+    });
+}
+
+function getTwoHoursAgo() {
+  const twoHoursAgo = new Date();
+  twoHoursAgo.setHours(twoHoursAgo.getHours() - 2);
+  return twoHoursAgo.toISOString();
+}
+
+function fetchVideoStats(videoIds) {
+  const apiUrl = `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoIds}&key=${apiKey}`;
+
+  return fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => data.items)
+    .catch((error) => {
+      console.error("Error fetching video stats:", error);
+      return [];
+    });
+}
